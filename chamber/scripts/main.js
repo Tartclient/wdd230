8ec21420c181b58b8b965c7d52be92bf225c109c
Tartclient <henry.ariasguerrero@gmail.com>
2023-01-29 06:31:13 -0800

    bug fixed finally I can sleep

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,9 +1,9 @@
+const currentYear = new Date().getFullYear();
 /* copy right */
 const copyright = document.querySelector('#copyright')
 copyright.textContent = `©️ ${currentYear}`;
 
 /* Last Update */
-const currentYear = new Date().getFullYear();
 const lastModDate = document.querySelector('#lastModDate')
 const lastMod = new Date(document.lastModified);
 lastModDate.textContent = `Last Updated: ${lastMod.toLocaleDateString("en-US")} ${lastMod.toLocaleTimeString()}`;
@@ -14,11 +14,3 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
-
-/* Responsive Menu */
-function toggleMenu() {
-    document.querySelector("#mainNav").classList.toggle("open");
-}
-
-const bttn = document.getElementById("hmbrgr-bttn")
-bttnx.onclick = toggleMenu;
