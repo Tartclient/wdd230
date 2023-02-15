65075d1f078c1326b31bb8bf025c2070cde93a7b
Tartclient <henry.ariasguerrero@gmail.com>
2023-02-15 15:41:55 -0800

    image update

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -26,6 +26,8 @@ hambutton.addEventListener('click', () => {
 
 });
 
+document.querySelector('navLink')
+
 /* Join Weekday Banner */
 const dayOfWeek = now.getDay();
 const msg = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
