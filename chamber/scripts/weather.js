531e4c5fa6b3445c3d8bba8efae625d15b2b68cc
Tartclient <henry.ariasguerrero@gmail.com>
2023-03-15 18:40:50 -0700

    weather update

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -2,11 +2,13 @@
 const currentTemp = document.querySelector('#current-temp')
 const weatherIcon = document.querySelector('#weather-icon')
 const captionDesc = document.querySelector('figcaption')
+const windSpeed = document.querySelector('#wind-speed');
+const windChill = document.querySelector('#wind-chill');
 
 //weather api key for easy change
 const key='4def50accbd787e425567d862bd5108f'
 
-const url=`https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=${key}`
+const url=`https://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&appid=${key}`
 
 async function apiFetch() {
     try {
@@ -26,12 +28,19 @@ async function apiFetch() {
 apiFetch()
 
 function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`
-    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`
-    const desc = weatherData.weather[0].description
-    weatherIcon.setAttribute('src', iconsrc)
-    weatherIcon.setAttribute('alt', desc)
-    captionDesc.textContent = desc
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+
+    windSpeed.textContent = `Wind Speed: ${weatherData.wind.speed.toFixed(1)} mph`;
+
+    const temperature = weatherData.main.temp;
+    const speed = weatherData.wind.speed;
+    const windChillValue = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temperature * Math.pow(speed, 0.16));
+    windChill.textContent = `Wind Chill: ${windChillValue.toFixed(1)}°F`;
 }
 
 
