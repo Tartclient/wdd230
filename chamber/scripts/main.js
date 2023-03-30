c27e0086663132ce67807aae73ba5b950a916961
Tartclient <henry.ariasguerrero@gmail.com>
2023-03-30 16:30:39 -0700

    Join update

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -51,3 +51,16 @@ function handleSubmit(event) {
 	form.reset();
 }
 
+/* Join message */
+function handleSubmit(event) {
+	event.preventDefault(); // Prevent the form from being submitted and the page from refreshing
+  
+	// Display the thank you message
+	const submitMessage = document.getElementById("submit-message");
+	submitMessage.textContent = "Thank you for submitting!";
+  
+	// Clear the form fields
+	const form = event.target;
+	form.reset();
+}
+
